e09f49bdfdc132620ee71f6efce4ea464c6d0ed0
yjjo <wnsdl9331@gmail.com>
2026-06-10 12:12:08 +0900

    fix: MeetingView useRef/useCallback import 누락 수정

--- src/components/MeetingView.tsx
+++ src/components/MeetingView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { salesDb as db, salesDb } from '../firebase';
 import {
   collection, getDocs, doc, setDoc, deleteDoc, orderBy, query, where,
